1ef13423fe0d9a02bc7bcf0fc9253f60c6eede4b
aboynejames <aboynejames@gmail.com>
2026-04-14 21:09:13 +0100

    route of lifepattern and bentopatterns

--- src/index.js
+++ src/index.js
@@ -1,134 +1,129 @@
-'use strict'
+"use strict";
 /**
-*  
-*  conduct local ML
-*
-* @class HopLearn
-* @package    HopLearn
-* @copyright  Copyright (c) 2024 James Littlejohn
-* @license    http://www.gnu.org/licenses/old-licenses/gpl-3.0.html
-* @version    $Id$
-*/
-import EventEmitter from 'events'
-import LifeMapping from './lifepatterns/magneticMapper.js'
+ *
+ *  conduct local ML
+ *
+ * @class HopLearn
+ * @package    HopLearn
+ * @copyright  Copyright (c) 2024 James Littlejohn
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-3.0.html
+ * @version    $Id$
+ */
+import EventEmitter from "events";
+import LifeMapping from "./lifepatterns/magneticMapper.js";
 
 class HopLearn extends EventEmitter {
-
   constructor() {
-    super()
-    this.activeList = []
-    this.caleEvolution = {}
-    this.lifeMapper = new LifeMapping()
-    this.LLMlocal = {}
+    super();
+    this.activeList = [];
+    this.caleEvolution = {};
+    this.lifeMapper = LifeMapping;
+    this.LLMlocal = {};
   }
 
   /**
-  * route to interplay patterns
-  * @method lifeFlow
-  *
-  */
+   * route to interplay patterns
+   * @method lifeFlow
+   *
+   */
   lifeFlow = function (story, pattern) {
-    let lifePattern = this.lifeMapper.mapStoryTopattern(story, pattern)
-    return lifePattern
-  }
+    let lifePattern = this.lifeMapper.mapStoryTopattern(story, pattern);
+    return lifePattern;
+  };
 
   /**
-  * connect to local ML's default
-  * @method openAgent
-  *
-  */
+   * connect to local ML's default
+   * @method openAgent
+   *
+   */
   openAgent = async function (agent) {
     // need a dynamtic way to do this, just like system in ECS.
-    if (agent.agent === 'cale-evolution') {
-      this.learnListeners()
-    } else if (agent.agent === 'cale-gpt4all') {
+    if (agent.agent === "cale-evolution") {
+      this.learnListeners();
+    } else if (agent.agent === "cale-gpt4all") {
       // get default LLM Model and start
       // match model type TODO get more detail info on setup e.g. gpu cpu
     } else {
-      console.log('no agent sorry')
+      console.log("no agent sorry");
     }
-  }
+  };
 
   /**
-  * stop to local ML's default
-  * @method closeOrchestra
-  *
-  */
+   * stop to local ML's default
+   * @method closeOrchestra
+   *
+   */
   closeOrchestra = function (agent) {
     // need a dynamtic way to do this, just like system in ECS.
-    if (agent.agent === 'cale-evolution') {
+    if (agent.agent === "cale-evolution") {
       // send message to beebee to ask peer to start agent
-      let outFlow = {}
-      outFlow.type = 'hop-learn'
-      outFlow.action = 'cale-evolution'
-      outFlow.task = 'closed'
-      outFlow.data = { name: 'cale-evolution', status: 'closed'}
-      this.emit('hop-learn', outFlow)
-    } else if (agent.agent === 'cale-gpt4all') {
+      let outFlow = {};
+      outFlow.type = "hop-learn";
+      outFlow.action = "cale-evolution";
+      outFlow.task = "closed";
+      outFlow.data = { name: "cale-evolution", status: "closed" };
+      this.emit("hop-learn", outFlow);
+    } else if (agent.agent === "cale-gpt4all") {
       // blunt need to close model but remain open for other model selection TODO
       // this.LLMlocal.removeAllListeners()
       // this.LLMlocal = {}
-      let outFlow = {}
-      outFlow.type = 'hop-learn'
-      outFlow.action = 'cale-gpt4all'
-      outFlow.task = 'closed'
-      outFlow.data = { name: 'cale-gpt4all', model: agent.model, status: 'closed'}
-      this.emit('hop-learn', outFlow)
+      let outFlow = {};
+      outFlow.type = "hop-learn";
+      outFlow.action = "cale-gpt4all";
+      outFlow.task = "closed";
+      outFlow.data = {
+        name: "cale-gpt4all",
+        model: agent.model,
+        status: "closed",
+      };
+      this.emit("hop-learn", outFlow);
     } else {
-      console.log('no agent sorry')
+      console.log("no agent sorry");
     }
-  }
+  };
 
   /**
-  * coordinate the to right AI
-  * @method coordinateAgents
-  *
-  */
+   * coordinate the to right AI
+   * @method coordinateAgents
+   *
+   */
   coordinateAgents = async function (message) {
     // check agent is active
-    let activeCheck = false
+    let activeCheck = false;
     for (let agent of this.activeList) {
-      if (typeof agent === 'object') {
-        activeCheck = true
+      if (typeof agent === "object") {
+        activeCheck = true;
       }
     }
     if (activeCheck === true) {
-      if (message.task === 'cale-evolution') {
-
-      } else if (message.action === 'question') {
-
-      } else if (message.task === 'cale-gpt4all-rag') {
-
-      } else if (message.task === 'llm-timeseries') {
-
+      if (message.task === "cale-evolution") {
+      } else if (message.action === "question") {
+      } else if (message.task === "cale-gpt4all-rag") {
+      } else if (message.task === "llm-timeseries") {
       }
     } else {
       // send message to beebee to ask peer to start agent
-      let messageOut = {}
-      messageOut.type = 'bbai-reply'
-      messageOut.action = 'hop-learn-feedback'
-      messageOut.data = { agent: 'not-active', input: message }
-      this.emit('hop-learn', messageOut)
+      let messageOut = {};
+      messageOut.type = "bbai-reply";
+      messageOut.action = "hop-learn-feedback";
+      messageOut.data = { agent: "not-active", input: message };
+      this.emit("hop-learn", messageOut);
     }
-
-  }
-
-  /**
-  * listen for message back to BeeBee
-  * @method learnListeners
-  *
-  */
-  learnListeners = function () {
-  }
+  };
 
   /**
-  * listen for message back to BeeBee
-  * @method learnListenersLLM
-  *
-  */
-  learnListenersLLM = function () {
-  }
+   * listen for message back to BeeBee
+   * @method learnListeners
+   *
+   */
+  learnListeners = function () {};
 
+  /**
+   * listen for message back to BeeBee
+   * @method learnListenersLLM
+   *
+   */
+  learnListenersLLM = function () {};
 }
 
-export default HopLearn
+export default HopLearn;
